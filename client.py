5a92fa32e39a09e6016559f394d7eebf11ab4824
James McHargue <jpm221@case.edu>
2022-02-09 22:55:58 -0500

    Finished assignment.

--- client.py
+++ client.py
@@ -1,13 +1,45 @@
+import os
 import socket
 import sys
 
+def isValidIP2(str):
+    check = os.system(f'ping -c 1 {str} > /dev/null')
+    return True if check == 0 else False
+
+def isValidIP(str):
+    if len(str) == 0:
+        return False
+    numbers = str.split('.')
+    if len(numbers) != 4:
+        return False
+    for i in numbers:
+        if len(i) > 3 or not i.isnumeric():
+            return False
+    return True
+
 print("Welcome to the KeyValue Service client!")
-ip = input("Please enter the IP or hostname of the server: ")
+
+ip = ""
+while True:
+    ip = input("Please enter the IP or hostname of the server: ")
+    if isValidIP2(ip):
+        break
+    print(f"'{ip}' is not a valid IP or hostname")
+print(f"Connecting to {ip}...")
+
 PORT = 50000
 
 # Open a TCP connection
+hello = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hello.connect((ip, PORT))
+assignedPort = int(hello.recv(1024).decode())
+hello.close()
+
+print(f"Assigned to Port {assignedPort}...")
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect((ip, PORT))
+connection.connect((ip, assignedPort))
+
+print("Connected!")
 
 valid_commands = [
     'get',
@@ -26,12 +58,21 @@ def send(string):
 def process(command):
     args = command.split(' ')
     if len(args) > 0:
-        if args[0] in valid_commands:
+        if args[0] == "help":
+            print("help \n \
+                get key \
+                put key value \
+                values \
+                keyset \
+                mappings \
+                bye")
+        elif args[0] == "bye":
+            send(command)
+            quit()
+        elif args[0] in valid_commands:
             print(send(command))
         else:
             print(f"Invalid command '{command}'")
-
-
 
 while True:
     command = input("KeyValue Service> ")
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 PORT = 50000
 
@@ -11,49 +12,83 @@ map = {}
 
 def parse(command):
     args = [s for s in command.split(' ') if len(s) > 0]
-    if args[0] == "get":
-        if len(args) < 2:
-            return "Invalid usage of get: must provide key"
-        key = ' '.join(args[1:])
-        if key not in map:
-            return f"Cannot find {key} in data store"
-        else:
-            return map[key]
+    if len(args) > 0:
+        if args[0] == "get":
+            if len(args) < 2:
+                return "Invalid usage of get: must provide key"
+            key = ' '.join(args[1:])
+            if key not in map:
+                return f"Cannot find {key} in data store"
+            else:
+                return map[key]
 
-    elif args[0] == "put":
-        if len(args) < 3:
-            return "Invalid usage of put: must provide key and value"
-        if not isnumeric(args[-1]):
-            return f"Value {args[-1]} is not valid"
-        key = ' '.join(args[1:-1])
-        map[key] = args[-1]
+        elif args[0] == "put":
+            if len(args) < 3:
+                return "Invalid usage of put: must provide key and value"
+            if not args[-1].isnumeric():
+                return f"Value {args[-1]} is not valid"
+            key = ' '.join(args[1:-1])
+            map[key] = args[-1]
+            return "Success"
 
-    elif args[0] == "mappings":
-        acc = ""
-        for key in map:
-            acc += key + ' ' + map + '\n'
-        return acc
+        elif args[0] == "mappings":
+            if len(map.keys()) == 0:
+                return "no mappings assigned"
+            acc = ""
+            for key in map:
+                acc += key + ' ' + map[key] + '\n'
+            return acc[:-1]
 
-    elif args[0] == "keyset":
-        acc = ""
-        for key in map:
-            acc += key + '\n'
-        return acc
+        elif args[0] == "keyset":
+            if len(map.keys()) == 0:
+                return "no keys assigned"
+            acc = ""
+            for key in map:
+                acc += key + '\n'
+            return acc[:-1]
 
-    elif args[0] == "values":
-        acc = ""
-        for key in map:
-            acc += map[key] + '\n'
-        return acc
+        elif args[0] == "values":
+            if len(map.keys()) == 0:
+                return "no values assigned"
+            acc = ""
+            for key in map:
+                acc += map[key] + '\n'
+            return acc[:-1]
+
+def runService(local, port):
+    client, addr = local.accept()
+    while True:
+        try:
+            data = client.recv(1024) # Receive data
+            if data:
+                command = data.decode()
+                if command == "bye":
+                    print(f"Closing connection on port {port}")
+                    return
+                response = parse(command)
+                client.send(response.encode())
+        except ConnectionResetError:
+            print(f"Connection lost with client on port {port}...")
+            return
 
 
+nextPort = PORT + 1
+
+heard = False
 while True:
     # Constantly listen for connecting clients
-    arrivingSocket, addr = connection.accept()
-    data = arrivingSocket.recv(1024)
+    client, addr = connection.accept()
+    print(f"Received connection from {addr}, assigning to port {nextPort}")
+    if addr: # We've received a connection!
+        local = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        local.bind(('', nextPort))
+        local.listen(1)
+        client.send(str(nextPort).encode())
+
+        threading.Thread(target=runService, args=(local, nextPort)).start()
+        nextPort += 1
 
     # Pass the client's message to parse(), then send back what parse() returns
-    arrivingSocket.send(parse(data.decode()).encode())
 
     # Close the connection to the client
     #arrivingSocket.close()
